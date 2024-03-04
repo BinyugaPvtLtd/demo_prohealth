d4fbf0b976998dc0a30085bb42dd402af5f9e742
pachangeprachi <85255535+pachangeprachi@users.noreply.github.com>
2024-03-04 14:41:43 +0530

    Merge remote-tracking branch 'origin/prachi' into prachi
    # Conflicts:
    #	lib/tab_screen/tab_screen.dart
    #	lib/web_screen/web_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:demo_prohealth/controller.dart';
-import 'package:demo_prohealth/tab_screen/tab_screen.dart';
-import 'package:demo_prohealth/web_screen/web_screen.dart';
+import 'package:demo_prohealth/web_screen/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -18,38 +17,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      initialRoute: "/",
       home: MyHomePage(
         controller: myController,
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final MyController controller;
-
-  MyHomePage({required this.controller});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Responsive App'),
-      ),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          double screenWidth = constraints.maxWidth;
-
-          // Update the screen type in the controller
-          controller.checkScreenType(screenWidth);
-
-          // Use the isTabletScreen flag to determine the screen type
-          if (controller.isTabletScreen.value) {
-            return TabletScreen();
-          } else {
-            return DesktopWebScreen();
-          }
-        },
       ),
     );
   }
--- lib/web_screen/web_screen.dart
+++ lib/web_screen/web_screen.dart
@@ -3,31 +3,52 @@ import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
 
-class DesktopWebScreen extends StatelessWidget {
+class DesktopWebScreen extends StatefulWidget {
+  @override
+  State<DesktopWebScreen> createState() => _DesktopWebScreenState();
+}
+
+class _DesktopWebScreenState extends State<DesktopWebScreen> {
   final MyController myController = Get.find();
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Desktop Web Screen'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Desktop Web Screen Content',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Desktop Web Button'),
-            ),
-          ],
-        ),
-      ),
-    );
+        body: Column(children: [
+      Expanded(
+          child: Container(
+        color: Colors.purpleAccent,
+      )),
+      Expanded(
+          child: Container(
+        color: Colors.pinkAccent,
+      )),
+      // Container(
+      //   height: 40,
+      //   width: 700,
+      //   padding: EdgeInsets.all(5),
+      //   decoration: BoxDecoration(
+      //       borderRadius: BorderRadius.circular(30),
+      //       color: Color(0xff50B5E5)),
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //     children: [
+      //       Text('Employment'),
+      //       Text('Education'),
+      //       Text('Referance'),
+      //       Text('Licenses'),
+      //     ],
+      //   ),
+      // )
+      // Container(
+      //   color: Color(0xff50B5E5),
+      //   child: _buildBottomBar(),
+      // )
+    ]));
   }
+
+  // Widget _buildBottomBar() {
+  //   return CustomAnimatedBottomBar();
+  // }
 }
